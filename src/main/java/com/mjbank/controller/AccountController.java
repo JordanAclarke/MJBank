848694cf95433f6809ace104b61dfdd2c76ab521
Mohamedzaeem <mohamedmomin5994@gmail.com>
2020-01-20 10:28:45 -0500

    add account Fixed

--- src/main/java/com/mjbank/controller/AccountController.java
+++ src/main/java/com/mjbank/controller/AccountController.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.google.gson.Gson;
 import com.mjbank.model.Account;
 import com.mjbank.service.AccountService;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class AccountController {
@@ -109,6 +111,7 @@ public class AccountController {
 		accountService.deleteAccount(id);
 		return "Account Deleted";
 	}
+
 	
 	
 
